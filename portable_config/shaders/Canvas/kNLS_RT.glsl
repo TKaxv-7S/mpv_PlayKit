4574f74d5cd2bc8e5dbb10574a0067ec1744e0d3
hooke007 <hooke007@qq.com>
2025-12-16 00:30:34 +0800

    着色器： kNLS 增加另一种模式

--- portable_config/shaders/Canvas/kNLS_RT.glsl
+++ portable_config/shaders/Canvas/kNLS_RT.glsl
@@ -1,6 +1,12 @@
 // 文档 https://github.com/hooke007/mpv_PlayKit/wiki/4_GLSL
 
 
+//!PARAM MODE
+//!TYPE DEFINE
+//!MINIMUM 1
+//!MAXIMUM 2
+1
+
 //!PARAM CTP
 //!TYPE float
 //!MINIMUM 0.0
@@ -19,26 +25,25 @@
 //!DESC [kNLS_RT]
 //!WHEN STR
 
-float nonlinear_map(float x, float stretch_factor, float center_w, float pwr) {
+float nonlinear_map(float x, float stretch_factor) {
+#if MODE == 1
+
 	float sign_x = sign(x);
 	float abs_x = abs(x);
-	float blend = smoothstep(0.0, 1.0, abs_x / center_w);
-	if (abs_x > center_w) blend = 1.0;
-	float center_source = abs_x * stretch_factor;
 	float source_x;
 
-	if (abs_x <= center_w) {
+	if (abs_x <= CTP) {
 		source_x = abs_x * stretch_factor;
 	} else {
-		float center_src = center_w * stretch_factor;
+		float center_src = CTP * stretch_factor;
 		if (center_src >= 1.0) {
 			source_x = 1.0;
 		} else {
-			float t = (abs_x - center_w) / (1.0 - center_w);
+			float t = (abs_x - CTP) / (1.0 - CTP);
 			float src_range = 1.0 - center_src;
-			float required_slope = stretch_factor * (1.0 - center_w) / src_range;
+			float required_slope = stretch_factor * (1.0 - CTP) / src_range;
 			float base_n = required_slope;
-			float n = clamp(base_n * pwr, 0.3, 6.0);
+			float n = clamp(base_n * STR, 0.3, 6.0);
 			float g = 1.0 - pow(1.0 - t, n);
 			source_x = center_src + src_range * g;
 		}
@@ -46,6 +51,15 @@ float nonlinear_map(float x, float stretch_factor, float center_w, float pwr) {
 
 	source_x = clamp(source_x, 0.0, 1.0);
 	return sign_x * source_x;
+
+#elif MODE == 2
+
+	float k = stretch_factor - 1.0;
+	float blend = 1.0 - x * x;
+	float source_x = x * (1.0 + blend * k * STR);
+	return clamp(source_x, -1.0, 1.0);
+
+#endif
 }
 
 vec4 hook() {
@@ -62,11 +76,11 @@ vec4 hook() {
 
 	if (aspect_ratio > 1.0) {
 		float x = pos.x * 2.0 - 1.0;
-		float source_x = nonlinear_map(x, aspect_ratio, CTP, STR);
+		float source_x = nonlinear_map(x, aspect_ratio);
 		sample_pos.x = source_x * 0.5 + 0.5;
 	} else {
 		float y = pos.y * 2.0 - 1.0;
-		float source_y = nonlinear_map(y, 1.0 / aspect_ratio, CTP, STR);
+		float source_y = nonlinear_map(y, 1.0 / aspect_ratio);
 		sample_pos.y = source_y * 0.5 + 0.5;
 	}
 
